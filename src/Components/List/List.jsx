efd3070f60d86b2385fa0984c22c47ca20dbf21a
Prachi Gore <prachigore408@gmail.com>
2023-04-25 23:04:36 +0530

    list updated

--- src/Components/List/List.jsx
+++ src/Components/List/List.jsx
@@ -1,25 +1,50 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './list.scss'
 import ArrowBackIosNewOutlinedIcon from '@mui/icons-material/ArrowBackIosNewOutlined';
 import ArrowForwardIosOutlinedIcon from '@mui/icons-material/ArrowForwardIosOutlined';
 import ListItem from "../ListItem/ListItem"
+import { useRef } from 'react';
 
-const list = () => {
+const List = () => {
+  const[isMoved,setIsMoved]=useState(false);
+const[slideNumber, setSlideNumber]=useState(0)
+
+
+  const listRef=useRef()
+  const handleClick=(direction)=>{
+    setIsMoved(true)
+    let distance=listRef.current.getBoundingClientRect().x
+    if(direction==="left"&&slideNumber>0){
+      setSlideNumber(slideNumber-1)
+listRef.current.style.transform=`translateX(${230+distance}px)`
+    }
+    if(direction==="right"&&slideNumber<4){
+      setSlideNumber(slideNumber+1)
+      listRef.current.style.transform=`translateX(${-230+distance}px)`
+          }
+  }
   return (
     <div className='list'>
      <span className="listTitle">Continue to watch</span>
      <div className="wrapper">
-<ArrowBackIosNewOutlinedIcon/>
-<div className="container">
-    <ListItem />
-    <ListItem />
-    <ListItem />
+<ArrowBackIosNewOutlinedIcon className='sliderArrow left' onClick={()=>handleClick("left")} style={{display:!isMoved && "none"}}/>
+<div className="container" ref={listRef}>
+    <ListItem index={0}/>
+    <ListItem index={1}/>
+    <ListItem index={2}/>
+    <ListItem index={3}/>
+    <ListItem index={4}/>
+    <ListItem index={5}/>
+    <ListItem index={6}/>
+    <ListItem index={7}/>
+    <ListItem index={8}/>
+    <ListItem index={9}/>
 
 </div>
-<ArrowForwardIosOutlinedIcon/>
+<ArrowForwardIosOutlinedIcon  className='sliderArrow right' onClick={()=>handleClick("right")}/>
      </div>
     </div>
   )
 }
 
-export default list
+export default List
