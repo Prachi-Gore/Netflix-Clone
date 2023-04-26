d210e7a46ce33372bf3d4801469ca6fe8fa8ca10
Prachi Gore <prachigore408@gmail.com>
2023-04-26 22:41:30 +0530

    register page

---
+++ src/Pages/Register/Register.jsx
@@ -0,0 +1,52 @@
+import React, { useRef } from 'react'
+import './register.scss'
+import { useState } from 'react'
+
+const Register = () => {
+    const[email,setEmail]=useState("")
+
+    const emailRef=useRef()
+   const handleStart=()=>{
+setEmail(emailRef.current.value)
+   }
+   const[password,setPassword]=useState("")
+
+    const passwordRef=useRef()
+   const handleFinish=()=>{
+setPassword(passwordRef.current.value)
+   }
+  return (
+    <div className='register'>
+        <div className="top">
+            <div className="wrapper">
+            <img className='logo' src='https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/Netflix_2015_logo.svg/2560px-Netflix_2015_logo.svg.png'></img>
+            <button className="signinButton">Sign In</button>
+            </div>
+        </div>
+        <div className="container">
+            <h1>Unlimited movies, TV shows, and more.</h1>
+            <h2>Watch anywhere. Cancel anytime.</h2>
+            <p>
+                Ready to watch? Enter your email to create or restart your membership.
+            </p>
+            {
+            !email ?
+            <div className="input">
+                <input type="email" placeholder='email address' ref={emailRef}   
+                />
+                <button className="registeredButton"onClick={handleStart}>Get Started</button>
+            </div>:
+            <form className="input">
+            <input type="password" placeholder='password' ref={passwordRef}   
+            />
+            <button className="registeredButton"onClick={handleFinish}> Start</button>
+        </form>
+
+}
+        </div>
+      
+    </div>
+  )
+}
+
+export default Register
