7e079cec1f7c1b411d1bc8841a0429e799de6adb
Prachi Gore <prachigore408@gmail.com>
2023-04-26 22:41:46 +0530

    UI created

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,30 @@
-
+import {BrowserRouter as Router,Routes,Route} from "react-router-dom";
+import Login  from "./Pages/Login/Login"
+import Home  from "./Pages/Home/Home";
+import Register from "./Pages/Register/Register"
+import Watch from "./Pages/Watch/Watch"
 import './App.scss';
 
-import Home from './Home/Home';
+
+
 
 const App=()=> {
   return (
-    <div className='appdiv'>
+    
+     <Router>
+   <div className='appdiv'>
      
-      <Home/>
+      <Routes>
+        <Route path='/' element={<Home />}></Route>
+        <Route path='/login' element={<Login/>}></Route>
+        <Route path='/register' element={<Register/>}></Route>
+        <Route path='/watch' element={<Watch/>}></Route>
+       
+      </Routes>
+   </div>
+     </Router>  
      
-    </div>
+   
   );
 }
 
