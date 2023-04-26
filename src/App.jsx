fa77f6a8645e48a89809ba567dc39874526c785f
Prachi Gore <prachigore408@gmail.com>
2023-04-27 00:14:55 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -13,7 +13,7 @@ const App=()=> {
     
      <Router>
    <div className='appdiv'>
-     HHHHH
+    
       <Routes>
         <Route path='/' element={<Home />}></Route>
         <Route path='/login' element={<Login/>}></Route>
